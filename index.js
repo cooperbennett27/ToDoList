6cd99229f27c86dc784e47b981c8373396e5c3df
syffin <cooperbennett327@gmail.com>
2025-10-20 18:46:07 -0400

    functionality to add elements

---
+++ index.js
@@ -0,0 +1,20 @@
+const list = document.getElementById("list");
+const textInput = document.getElementById("list-text-input");
+const dateInput = document.getElementById("list-date-input");
+
+const addBtn = document.getElementById("add-btn");
+
+addBtn.addEventListener("click", function addElement() {
+    createElement(textInput.value, dateInput.value);
+})
+
+function createElement(text, date){
+    let html = `<li class="list-element">
+                    <p class="list-text roboto-normal">${text}</p>
+                    <p class="list-date roboto-normal">${date}</p>
+                    <button class="list-btn">DONE</button>
+                </li>`
+    const templateElement = document.createElement("template");
+    templateElement.innerHTML = html;
+    list.appendChild(templateElement.content.firstChild);
+}
